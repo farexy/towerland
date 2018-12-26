5ecc3c2341e53a4c3f78aa2bd2ce43f8f6ae7a35
Farexy <farexy@mail.ru>
2018-12-27 00:35:25 +0200

    fixed special effect clone

--- Towerland.GameServer.Models/Effects/SpecialEffect.cs
+++ Towerland.GameServer.Models/Effects/SpecialEffect.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using Newtonsoft.Json;
 
 namespace Towerland.GameServer.Models.Effects
 {
-  public class SpecialEffect
+  public class SpecialEffect : ICloneable
   {
     [JsonProperty("i")] public EffectId Id { set; get; }
     [JsonProperty("d")] public int Duration { set; get; }
     [JsonProperty("e")] public double EffectValue { get; set; }
 
     public static SpecialEffect Empty { get; } = new SpecialEffect{Id = EffectId.None, Duration = 0};
+    
+    public object Clone()
+    {
+      return new SpecialEffect
+      {
+        Id = Id,
+        Duration = Duration,
+        EffectValue = EffectValue
+      };
+    }
   }
 }
--- Towerland.GameServer.Models/GameObjects/GameObject.cs
+++ Towerland.GameServer.Models/GameObjects/GameObject.cs
@@ -58,7 +58,7 @@ namespace Towerland.GameServer.Models.GameObjects
         Position = Position,
         Type = Type,
         WaitTicks = WaitTicks,
-        Effect = new SpecialEffect{Id = Effect.Id, Duration = Effect.Duration}
+        Effect = (SpecialEffect)Effect.Clone(),
       };
     }
 
