618e5b92cb3ba997e898e51c1df4b7f3e21186ce
Farexy <farexy@mail.ru>
2019-11-03 20:19:06 +0200

    added static api for public urls issuing

---
+++ Towerland.Static.Api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Towerland.Static.Api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IOptionsMonitor<ValuesConfig> _options;
+
+        public ValuesController(IOptionsMonitor<ValuesConfig> options)
+        {
+            _options = options;
+        }
+
+        [HttpGet("gsurl")]
+        public ActionResult<string> Get() => _options.CurrentValue.GsUrl;
+    }
+}
---
+++ Towerland.Static.Api/Models/ValuesConfig.cs
@@ -0,0 +1,7 @@
+namespace Towerland.Static.Api
+{
+  public class ValuesConfig
+  {
+    public string GsUrl { get; set; }
+  }
+}
